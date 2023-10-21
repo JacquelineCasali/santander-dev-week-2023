fb63d352d096e26b4af58fa3ead7acfcac9c4cfd
Jacqueline <jcasalia@yahoo.com.br>
2023-10-21 19:49:54 -0300

    implementação do dominio

---
+++ src/main/java/com/java/domain/model/Feature.java
@@ -0,0 +1,9 @@
+package com.java.domain.model;
+
+import jakarta.persistence.Entity;
+
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/com/java/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.java.domain.repository;
+
+import com.java.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
